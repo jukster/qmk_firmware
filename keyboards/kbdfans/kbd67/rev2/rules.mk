d6f9025b5ce48e53f44e3e1cbd14adc8dcbfc579
Marko Jukić <marko.jukic@gmail.com>
2020-12-07 05:33:26 +0000

    initial commit

--- keyboards/kbdfans/kbd67/rev2/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/rules.mk
@@ -31,5 +31,6 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
+CONFIG_USB_ACM = yes
 
 LAYOUTS = 65_ansi 65_iso 65_ansi_blocker 65_ansi_blocker_split_bs
