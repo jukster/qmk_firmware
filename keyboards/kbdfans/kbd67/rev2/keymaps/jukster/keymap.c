8e363015227c797634d6bc69b85bcbb5e6645e82
Marko Jukić <marko.jukic@gmail.com>
2020-12-10 05:55:07 +0000

    First step toward Slomak
    Changed the base layer character positions to act as ANSI
    when Slovenian layout is selected.
    
    Changed the mod layer to get Slovenian accented chars

--- keyboards/kbdfans/kbd67/rev2/keymaps/jukster/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/jukster/keymap.c
@@ -36,29 +36,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [0] = LAYOUT_65_ansi_split_bs_split_spc(
-  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_VOLD, KC_VOLU, KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,       KC_BSLS, KC_PGUP,    \
-  KC_BSPC, KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    KC_QUOT,                KC_ENT,  KC_PGDN,    \
-  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                KC_UP,   KC_DEL,     \
+  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_SLSH, S(KC_0),  KC_VOLD, KC_VOLU, KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,   KC_F,   KC_P,   KC_G,   KC_J,   KC_L,   KC_U,    KC_Y,    S(KC_DOT), LALT(KC_LBRC), LALT(KC_RBRC), LALT(KC_BSLS), KC_PGUP,    \
+  KC_BSPC, KC_A,    KC_R,   KC_S,   KC_T,   KC_D,   KC_H,   KC_N,   KC_E,    KC_I,    KC_O,    S(KC_7),                KC_ENT,  KC_PGDN,    \
+  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_K,   KC_M,   KC_COMM, KC_DOT,  KC_MINS, KC_RSFT,                KC_UP,   KC_DEL,     \
   KC_LCTL, KC_LALT, KC_LGUI,        KC_SPC,       MO(1),        KC_ENT,      KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT,       KC_DOWN, KC_RGHT),
 
   /* Keymap Fn Layer
    * ,----------------------------------------------------------------.
-   * |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+   * |~ `|   |   |   |   |   |   |   |   |   |   |   |Mute| Play |    |
    * |----------------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |PSc|SLk|Pau|Up |   |     |    |
+   * |     |   |   |   |   |   |   |   | Up |   | š  | ž | ć  |   |   |
    * |----------------------------------------------------------------|
-   * |      |Lef|Dow|Rig|   |   |   |   |Hom|PUp|Lef|Rig|        |    |
+   * |      |   |   |   |   |   |a(l)|lft|down|rght|a(r)| č |   |     |    
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |End|PDn|Dow|      |PUp|    |
+   * |        |   |   |   |   |   |   |   |   |   |   |      |PUp|    |
    * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |Hom|PDn|End |
+   * |    |    |    |                       |   |   |    |   |PDn|    |
    * `----------------------------------------------------------------'
    */
 [1] = LAYOUT_65_ansi_split_bs_split_spc(
    KC_GRV,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MUTE,KC_MPLY,_______, \
-       _______,_______,_______,_______,_______,_______,_______,_______,  KC_UP,_______,_______,_______,_______,    _______,_______, \
-         KC_CAPS,_______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,          _______,_______, \
+       _______,_______,_______,_______,_______,_______,_______,_______,  KC_UP,_______,KC_LBRC,KC_BSLS,KC_QUOT,    _______,_______, \
+         KC_CAPS,_______,_______,_______,_______,_______,LALT(KC_LEFT),KC_LEFT,KC_DOWN,KC_RGHT,LALT(KC_RGHT),KC_SCLN,_______,_______, \
              _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      _______,KC_PGUP,_______, \
      _______,_______,_______,        _______,    _______,     _______,             _______,_______,_______,_______,KC_PGDN,_______),
 
