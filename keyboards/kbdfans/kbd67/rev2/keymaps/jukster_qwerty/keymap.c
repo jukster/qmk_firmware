3acf2aa24353ec57e3bc762eb24ef52934fc40ab
Marko Jukić <marko.jukic@gmail.com>
2020-12-28 06:12:11 +0000

    Minimal adjustment for qwerty

--- keyboards/kbdfans/kbd67/rev2/keymaps/jukster_qwerty/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/jukster_qwerty/keymap.c
@@ -45,12 +45,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 
-[0] = LAYOUT_65_ansi_split_bs_split_spc(
+[0] = LAYOUT_65_ansi_split_bs(
   KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_SLSH, KC_EQL,  KC_VOLD, KC_VOLU, KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P, LALT(KC_LBRC), LALT(KC_RBRC), LALT(KC_BSLS), KC_PGUP,    \
   KC_BSPC, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    CU_SCLN,    CU_QUOT,                KC_ENT,  KC_PGDN,    \
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_MINS, KC_RSFT,                KC_UP,   KC_DEL,     \
-  KC_LCTL, KC_LALT, CU_LGUI,        KC_SPC,       TT(1),        KC_ENT,      KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT,       KC_DOWN, KC_RGHT),
+  KC_LCTL, KC_LALT, CU_LGUI,              KC_SPC,                    MO(1), KC_RALT, KC_RCTL, KC_LEFT,       KC_DOWN, KC_RGHT),
 
   /* Keymap Fn Layer
    * ,----------------------------------------------------------------.
@@ -65,13 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |      |       |        |   |   |    |   |PDn|    |
    * `----------------------------------------------------------------'
    */
-[1] = LAYOUT_65_ansi_split_bs_split_spc(
+[1] = LAYOUT_65_ansi_split_bs(
 
 _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_TOG,KC_MUTE,KC_MPLY,_______, \
        _______,_______,KC_BTN1,KC_MS_U,KC_BTN2,KC_WH_U,_______,KC_PGUP,  KC_UP,_______,KC_LBRC,KC_BSLS,KC_QUOT,_______,_______, \
          KC_CAPS,_______,KC_MS_L,KC_MS_D,KC_MS_R,KC_WH_D,LALT(KC_LEFT),KC_LEFT,KC_DOWN,KC_RGHT,LALT(KC_RGHT),KC_SCLN,_______,_______, \
              _______,KC_BSLS,KC_QUOT,KC_SCLN,_______,_______,_______,KC_PGDN,_______,_______,_______,      _______,KC_PGUP,_______, \
-     _______,_______,_______,        _______,    _______,     _______,             TG(1),_______,_______,_______,KC_PGDN,_______),
+     _______,_______,_______,           _______,              _______,_______,_______,_______,KC_PGDN,_______),
 
 };
 
